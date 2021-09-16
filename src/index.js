ba3238d147539242a88cfbccbbb28cd6a721f7ec
Sylvestre <pasytchangwa@gmail.com>
2021-09-16 20:03:31 +0300

    Modify index.js file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
+import store from './store/configureStore';
 import App from './App';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.getElementById('root'),
 );
