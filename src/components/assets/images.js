d461e6fe52de3261793e9132a0dc6b5b696499e3
Sylvestre <pasytchangwa@gmail.com>
2021-09-16 19:58:31 +0300

    Add assets directory

---
+++ src/components/assets/images.js
@@ -0,0 +1,19 @@
+import TSX from './TSX.png';
+import STOCK from './STOCK.png';
+import NYSE from './NYSE.png';
+import NASDAQ from './NASDAQ.jpg';
+import MUTUALFUND from './MUTUALFUND.png';
+import EURONEXT from './EURONEXT.png';
+import AMEX from './AMEX.jpg';
+
+const images = {
+  AMEX,
+  EURONEXT,
+  MUTUALFUND,
+  NASDAQ,
+  NYSE,
+  STOCK,
+  TSX,
+};
+
+export default images;
